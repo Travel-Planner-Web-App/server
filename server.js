3a47ef7a9895a906c864b9c9269bd0daa5b50ce7
EdwardThapelo <edwardthapelo55@gmail.com>
2025-01-16 23:29:23 +0200

    Inserted the Start server code

--- server.js
+++ server.js
@@ -28,3 +28,8 @@ app.use("/api/favorites", favoritesRoutes);
 module.exports = (req, res) => {
   app(req, res);
 };
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
